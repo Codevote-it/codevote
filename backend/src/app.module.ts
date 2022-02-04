fa5a7dfb145dce530a3ab1279cfcd3eb16d790c0
Martijn <martijn.brug@gmail.com>
2022-02-04 17:58:33 +0100

    feat(backend): pretteir and eslint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
-import { AppResolver } from "./app.resolver";
+import { AppResolver } from './app.resolver';
 
 @Module({
-    imports: [
-        GraphQLModule.forRoot({
-            playground: true,
-            typePaths: ['./**/*.graphql'],
-        }),
-    ],
-    providers: [AppService, AppResolver],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
+  providers: [AppService, AppResolver],
 })
-export class AppModule {
-}
+export class AppModule {}
--- backend/src/app.resolver.ts
+++ backend/src/app.resolver.ts
@@ -1,18 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { Args, Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class AppResolver {
-  constructor(
-  ) {}
-
   @Query()
   async codeVote(@Args('id') id: string) {
     return {
       snippet1: 'Some code snippetss 1',
       snippet2: 'Some code snippetss 2',
-      creator: 'martijn@oppedijk.online'
-    }
+      creator: 'martijn@oppedijk.online',
+    };
   }
 }

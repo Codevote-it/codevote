751211c91e0356d78f0e1d66e70fe87c293daee2
Martijn <martijn.brug@gmail.com>
2022-02-04 23:24:26 +0100

    feat(backend): more stuff commit

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker build -t eu.gcr.io/codevote/codevote-api .
 # Configure docker to use Google authentication
-
 gcloud auth configure-docker -q
 docker push eu.gcr.io/codevote/codevote-api

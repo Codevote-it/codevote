f9c81f9f55b2e6bd37de9ee57d04f288f768c4ae
Martijn <martijn.brug@gmail.com>
2022-02-04 23:41:47 +0100

    feat(backend): port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { logger } from "./logger";
 
+require('dotenv').config();
+const port = process.env.PORT! as unknown as number || 3000;
 async function bootstrap() {
-  require('dotenv').config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
-bootstrap();
+bootstrap().then(() =>
+    logger.log(`Started on port ${port}`)
+).catch(err => {
+  logger.error(err);
+});

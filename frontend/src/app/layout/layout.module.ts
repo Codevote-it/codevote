4bd8bc1a423fbb8215adece59b70e8cb6ae3200b
Moppedijk <social@oppedijk.online>
2022-02-08 17:42:42 +0100

    add app routing module

--- frontend/src/app/layout/layout.module.ts
+++ frontend/src/app/layout/layout.module.ts
@@ -4,12 +4,13 @@ import { HeaderComponent } from './header';
 import { FooterComponent } from './footer';
 import { ContainerComponent } from './container';
 import { ComponentsModule } from '@app/components';
+import { AppRoutingModule } from '@app/routing';
 
 const COMPONENTS = [HeaderComponent, FooterComponent, ContainerComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
   exports: [...COMPONENTS],
-  imports: [CommonModule, ComponentsModule],
+  imports: [CommonModule, ComponentsModule, AppRoutingModule],
 })
 export class LayoutModule {}

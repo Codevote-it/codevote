5e272e0ed83bc95c3eb095bea2a6dd0f01f8625c
Moppedijk <social@oppedijk.online>
2022-02-20 17:59:39 +0100

    lint warnings

--- frontend/src/app/pages/codevote/codevote.component.ts
+++ frontend/src/app/pages/codevote/codevote.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { saveTokenAction } from '@app/data';
 import {
   CodevoteActionService,
   CodevoteSelectorService,

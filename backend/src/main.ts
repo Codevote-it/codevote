2e276507f5d98981343ed1a47636f5177463bce4
Martijn <martijn.brug@gmail.com>
2022-04-29 19:47:41 +0200

    feat: npm and dotenv require

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './logger';
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 dotenv.config();
 const port = (process.env.PORT! as unknown as number) || 3000;
 async function bootstrap() {
